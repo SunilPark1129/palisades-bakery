3d07d54fd9b264cf89b3457428ce037e47042da5
jessicason0 <jessicajson0@gmail.com>
2025-11-12 17:12:59 -0500

    add: group order section

--- app/homepage/GroupOrder.tsx
+++ app/homepage/GroupOrder.tsx
@@ -1,7 +1,29 @@
+import Image from "next/image";
 import React from "react";
+import SectionHeader from "../_components/ui/SectionHeader";
+import groupOrderImg from "./images/bakery-group-img.jpg";
 
 function GroupOrder() {
-  return <div>GroupOrder</div>;
+  return (
+    <section className="flex flex-col gap-10 mx-auto">
+      <div className="">
+        <Image
+          src={groupOrderImg}
+          alt="Group & Bulk Orders"
+          className=" object-contain grayscale-65 w-[100rem] aspect-2/1"
+        />
+      </div>
+      <div className="flex flex-col gap-3 max-w-[36rem] mx-auto">
+        <SectionHeader title="Group & Bulk Bread Orders" />
+        <p>
+          Planning an office meeting or a gathering? We prepare assortments of
+          rolls, baguettes, pastries, and sandwich breads in bulk. Tell us the
+          quantity and pickup time, and we'll have everything fresh and ready.
+          Contact us for details.
+        </p>
+      </div>
+    </section>
+  );
 }
 
 export default GroupOrder;
