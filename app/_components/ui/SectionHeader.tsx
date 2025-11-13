c2172e92785cd9b4e6e2ce483e3b31acb682d5b3
Sunil Park <sunilpark1129@gmail.com>
2025-11-12 21:33:44 -0500

    edit: style

--- app/_components/ui/SectionHeader.tsx
+++ app/_components/ui/SectionHeader.tsx
@@ -5,7 +5,7 @@ type HeaderProps = {
 };
 
 function SectionHeader({ title }: HeaderProps) {
-  return <h2 className="text-3xl whitespace-nowrap">{title}</h2>;
+  return <h2 className="text-3xl min-w-fit">{title}</h2>;
 }
 
 export default SectionHeader;
--- app/homepage/GroupOrder.tsx
+++ app/homepage/GroupOrder.tsx
@@ -5,18 +5,20 @@ import groupOrderImg from "./images/bakery-group-img.jpg";
 function GroupOrder() {
   return (
     <div className="wrapper">
-      <section className="flex mx-auto p-4 gap-8">
-        <div className="flex flex-col justify-center gap-4 max-w-[36rem] mx-auto">
-          <SectionHeader title="Group & Bulk Bread Orders" />
-          <p className="max-w-[32rem]">
-            Planning an office meeting or a gathering? We prepare assortments of
-            rolls, baguettes, pastries, and sandwich breads in bulk. Tell us the
-            quantity and pickup time, and we'll have everything fresh and ready.
-            Contact us for details.
-          </p>
+      <section className="flex p-4">
+        <div className="flex-3">
+          <div className="flex flex-1 flex-col gap-4 p-4 h-full justify-center mx-auto max-w-[410px]">
+            <SectionHeader title="Group & Bulk Bread Orders" />
+            <p className="">
+              Planning an office meeting or a gathering? We prepare assortments
+              of rolls, baguettes, pastries, and sandwich breads in bulk. Tell
+              us the quantity and pickup time, and we'll have everything fresh
+              and ready. Contact us for details.
+            </p>
+          </div>
         </div>
 
-        <div className="relative w-full aspect-8/9">
+        <div className="relative flex-2 aspect-7/8">
           <Image
             src={groupOrderImg}
             alt="Group & Bulk Orders"
