7081e6f99519347f932e11f97a21abae900c5b61
jessicason0 <jessicajson0@gmail.com>
2025-11-13 17:18:55 -0500

    edit: block section styling

--- app/homepage/Block.tsx
+++ app/homepage/Block.tsx
@@ -3,18 +3,20 @@ import SectionHeader from "../_components/ui/SectionHeader";
 
 function Block() {
   return (
-    <section className="w-full flex justify-center bg-gray-100 pt-[10rem] pb-[10rem]">
-      <div className="max-w-[30rem] p-4 flex flex-col gap-4">
-        <h2 className="text-3xl">
-          Enjoy our freshly handcrafted{" "}
-          <span className="font-semibold">Cookies</span> &{" "}
-          <span className="font-semibold">Pies</span>
-        </h2>
-        <p>
-          Enjoy our freshly baked, handcrafted cookies made in-house. From
-          classic flavors to seasonal specials, each cookie is prepared with
-          quality ingredients for the perfect bite.
-        </p>
+    <section className="bg-gray-100 py-40">
+      <div className="wrapper">
+        <div className="flex flex-col gap-4 p-4 mx-auto max-w-[30rem]">
+          <h2 className="text-3xl">
+            Enjoy our freshly handcrafted{" "}
+            <span className="font-semibold">Cookies</span> &{" "}
+            <span className="font-semibold">Pies</span>
+          </h2>
+          <p>
+            Enjoy our freshly baked, handcrafted cookies made in-house. From
+            classic flavors to seasonal specials, each cookie is prepared with
+            quality ingredients for the perfect bite.
+          </p>
+        </div>
       </div>
     </section>
   );
