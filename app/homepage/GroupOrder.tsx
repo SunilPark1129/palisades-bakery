38a1094fb8c9ef99577cac2af83b09b51765383b
jessicason0 <jessicajson0@gmail.com>
2025-11-12 21:16:33 -0500

    edit: group order styling

--- app/homepage/GroupOrder.tsx
+++ app/homepage/GroupOrder.tsx
@@ -1,28 +1,31 @@
 import Image from "next/image";
-import React from "react";
 import SectionHeader from "../_components/ui/SectionHeader";
 import groupOrderImg from "./images/bakery-group-img.jpg";
 
 function GroupOrder() {
   return (
-    <section className="flex flex-col gap-[6rem] mx-auto max-w-[100rem] p-4 mb-[-1rem]">
-      <div className="">
-        <Image
-          src={groupOrderImg}
-          alt="Group & Bulk Orders"
-          className="object-contain grayscale-65 aspect-2/1"
-        />
-      </div>
-      <div className="flex flex-col gap-4 max-w-[36rem] mx-auto">
-        <SectionHeader title="Group & Bulk Bread Orders" />
-        <p>
-          Planning an office meeting or a gathering? We prepare assortments of
-          rolls, baguettes, pastries, and sandwich breads in bulk. Tell us the
-          quantity and pickup time, and we'll have everything fresh and ready.
-          Contact us for details.
-        </p>
-      </div>
-    </section>
+    <div className="wrapper">
+      <section className="flex mx-auto p-4 gap-8">
+        <div className="flex flex-col justify-center gap-4 max-w-[36rem] mx-auto">
+          <SectionHeader title="Group & Bulk Bread Orders" />
+          <p className="max-w-[32rem]">
+            Planning an office meeting or a gathering? We prepare assortments of
+            rolls, baguettes, pastries, and sandwich breads in bulk. Tell us the
+            quantity and pickup time, and we'll have everything fresh and ready.
+            Contact us for details.
+          </p>
+        </div>
+
+        <div className="relative w-full aspect-8/9">
+          <Image
+            src={groupOrderImg}
+            alt="Group & Bulk Orders"
+            fill
+            className="object-cover grayscale-65"
+          />
+        </div>
+      </section>
+    </div>
   );
 }
 
--- app/homepage/Hero.tsx
+++ app/homepage/Hero.tsx
@@ -5,7 +5,7 @@ function Hero() {
   return (
     <section className="flex flex-col">
       <h1 className="text-3xl text-center p-4">PALISADES PARK BAKERY</h1>
-      <div className="w-full aspect-16/9 max-h-[700px] min-h-[340px] relative">
+      <div className="w-full aspect-16/9 max-h-[600px] min-h-[340px] relative">
         <Image
           className="object-cover"
           src={heroImg}
