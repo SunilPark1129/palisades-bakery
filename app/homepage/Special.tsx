66780b7dcdd957f5cb42df639c3a87c1b8c5dd71
Sunil Park <sunilpark1129@gmail.com>
2025-12-08 08:54:42 -0500

    edit: style

--- app/homepage/Special.tsx
+++ app/homepage/Special.tsx
@@ -8,7 +8,7 @@ type Props = {};
 function Special({}: Props) {
   return (
     <div className="wrapper">
-      <section className="px-4 flex flex-col gap-4">
+      <section className="px-4 py-8 flex flex-col gap-4">
         <header className="flex flex-col gap-4">
           {/* <p className="text-xs uppercase tracking-wider">Special-Orders</p> */}
           <div className="flex gap-4 items-center">
