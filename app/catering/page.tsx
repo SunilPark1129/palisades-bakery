ef03cb75ef6dceb6c6fb87e954ce35522ed3dd2c
jessicason0 <jessicajson0@gmail.com>
2025-12-08 21:15:42 -0500

    add: custom cake descriptions

--- app/catering/page.tsx
+++ app/catering/page.tsx
@@ -4,161 +4,161 @@ type Props = {};
 
 function page({}: Props) {
   return (
-    <main>
-      <header>
-        <h1>Catering</h1>
-        <p>
-          We provide handcrafted baked goods made fresh for your event. Let us
-          take care of the treats while you enjoy the moment.
-        </p>
-      </header>
+    <div className="wrapper">
+      <main className="flex flex-col gap-6 p-4">
+        <header>
+          <h1 className="text-[1.4rem] font-bold">Catering</h1>
+          <p>
+            We provide handcrafted baked goods made fresh for your event. Let us
+            take care of the treats while you enjoy the moment.
+          </p>
+        </header>
 
-      <section>
-        <h2>Large Orders Made Easy</h2>
-        <p>
-          Whether you're planning a lunch, gathering, or special event, our
-          bakery offers flexible group ordering on any item we make — from
-          breads and cakes to brunch dishes and sandwiches.
-        </p>
-      </section>
+        <section>
+          <h2 className="text-[1.1rem] italic">Large Orders Made Easy</h2>
+          <p>
+            Whether you're planning a lunch, gathering, or special event, our
+            bakery offers flexible group ordering on any item we make — from
+            breads and cakes to brunch dishes and sandwiches.
+          </p>
+        </section>
 
-      <section>
-        <h2>Ordering Guidelines</h2>
-        <div>
-          <div>
-            <div>Order at least 2 days in advance</div>
-            <p>
-              This gives our team time to prepare fresh items in the quantities
-              you need.
-            </p>
-          </div>
-          <div>
-            <div>Place your order by phone or in-person</div>
-            <p>Just call or visit to speak with a staff member.</p>
-          </div>
-          <div>
-            <div>Provide basic information</div>
+        <section>
+          <h2 className="text-[1.1rem] italic">Ordering Guidelines</h2>
+          <div className="grid grid-cols-2 gap-4">
             <div>
-              <p>Items wanted</p>
-              <p>Quantity</p>
-              <p>Date & time of pickup</p>
-              <p>Any allergy or customization needs</p>
+              <div className="">Order at least 2 days in advance</div>
+              <p>
+                This gives our team time to prepare fresh items in the
+                quantities you need.
+              </p>
             </div>
-            <p></p>
+            <div>
+              <div>Place your order by phone or in-person</div>
+              <p>Just call or visit to speak with a staff member.</p>
+            </div>
+            <div>
+              <div>Provide basic information</div>
+              <ul className="list-disc list-inside pl-4">
+                <li>Items wanted</li>
+                <li>Quantity</li>
+                <li>Date & time of pickup</li>
+                <li>Any allergy or customization needs</li>
+              </ul>
+              <p></p>
+            </div>
+            <div>
+              <div>Pay in advance</div>
+              <p>
+                Once payment is confirmed, the order will be made ready for you.
+              </p>
+            </div>
+          </div>
+        </section>
+
+        <section>
+          <h2 className="text-[1.1rem] italic">Place Your Group Order Today</h2>
+          <p>We'll prepare everything fresh and ready for your event</p>
+          <div>Call or Visit Us Now</div>
+        </section>
+
+        {/* <section>
+          <h2>Our Catering Menu</h2>
+          <section>
+            <h3>Breakfast & Brunch</h3>
+            <ul>
+              <li>Assorted Croissants (Butter, Almond, Chocolate)</li>
+              <li>Muffin & Scone Platter</li>
+              <li>Bagels with Cream Cheese & Jam</li>
+              <li>Mini Danish Collection</li>
+            </ul>
+          </section>
+          <section>
+            <h3>Desserts & Pastries</h3>
+            <ul>
+              <li>Mini Cakes & Cupcakes</li>
+              <li>Assorted Cookies & Brownies</li>
+              <li>Seasonal Fruit Tarts</li>
+              <li>Custom Dessert Platters</li>
+            </ul>
+          </section>
+          <section>
+            <h3>Savory Options</h3>
+            <ul>
+              <li>Artisan Bread Basket</li>
+              <li>Sandwich & Slider Platters</li>
+              <li>Quiche & Savory Pastries</li>
+            </ul>
+          </section>
+          <div>
+            All items can be customized for allergies, dietary preferences, and
+            event size.
+          </div>
+        </section>
+        <section>
+          <h2>Why Choose Our Catering?</h2>
+          <ul>
+            <li>Baked Fresh Daily</li>
+            <li>Premium Ingredients</li>
+            <li>Custom Orders Welcome</li>
+            <li>Perfect for Any Event Size</li>
+          </ul>
+        </section>
+        <section>
+          <h2>Perfect For</h2>
+          <ul>
+            <li>Corporate Meetings</li>
+            <li>Weddings & Receptions</li>
+            <li>Birthday Parties</li>
+            <li>Baby Showers & Bridal Showers</li>
+            <li>School & Community Events</li>
+          </ul>
+        </section>
+        <section>
+          <h2>How It Works</h2>
+          <ol>
+            <li>Choose Your Menu - Select items from our menu page.</li>
+            <li>Place Your Order - Contact us directly through call.</li>
+            <li>We Bake & Prepare - Everything is baked fresh.</li>
+            <li>Pickup - Pickup your order from our store.</li>
+          </ol>
+        </section>
+        <section>
+          <div>
+            <h2>Catering Inquiry</h2>
+            <p>Have a special request or large order? We'd love to help.</p>
           </div>
           <div>
-            <div>Pay in advance</div>
-            <p>
-              Once payment is confirmed, the order will be made ready for you.
-            </p>
+            <div>Phone: (000) 000-0000</div>
+            <div>Address: 325 Broad Ave, Palisades Park, NJ 07650</div>
+            <div>Email: 000@gmail.com</div>
+            <div>Minimum Order: $50</div>
+            <div>Lead Time: 48 hours notice required.</div>
           </div>
-        </div>
-      </section>
-
-      <section>
-        <h2>Place Your Group Order Today</h2>
-        <p>We'll prepare everything fresh and ready for your event</p>
-        <div>
-          <button>Call Now</button>
-          <button>View Location</button>
-        </div>
-      </section>
-      {/* <section>
-        <h2>Our Catering Menu</h2>
-        <section>
-          <h3>Breakfast & Brunch</h3>
-          <ul>
-            <li>Assorted Croissants (Butter, Almond, Chocolate)</li>
-            <li>Muffin & Scone Platter</li>
-            <li>Bagels with Cream Cheese & Jam</li>
-            <li>Mini Danish Collection</li>
-          </ul>
         </section>
         <section>
-          <h3>Desserts & Pastries</h3>
-          <ul>
-            <li>Mini Cakes & Cupcakes</li>
-            <li>Assorted Cookies & Brownies</li>
-            <li>Seasonal Fruit Tarts</li>
-            <li>Custom Dessert Platters</li>
-          </ul>
-        </section>
-        <section>
-          <h3>Savory Options</h3>
-          <ul>
-            <li>Artisan Bread Basket</li>
-            <li>Sandwich & Slider Platters</li>
-            <li>Quiche & Savory Pastries</li>
-          </ul>
-        </section>
-        <div>
-          All items can be customized for allergies, dietary preferences, and
-          event size.
-        </div>
-      </section>
-      <section>
-        <h2>Why Choose Our Catering?</h2>
-        <ul>
-          <li>Baked Fresh Daily</li>
-          <li>Premium Ingredients</li>
-          <li>Custom Orders Welcome</li>
-          <li>Perfect for Any Event Size</li>
-        </ul>
-      </section>
-      <section>
-        <h2>Perfect For</h2>
-        <ul>
-          <li>Corporate Meetings</li>
-          <li>Weddings & Receptions</li>
-          <li>Birthday Parties</li>
-          <li>Baby Showers & Bridal Showers</li>
-          <li>School & Community Events</li>
-        </ul>
-      </section>
-      <section>
-        <h2>How It Works</h2>
-        <ol>
-          <li>Choose Your Menu - Select items from our menu page.</li>
-          <li>Place Your Order - Contact us directly through call.</li>
-          <li>We Bake & Prepare - Everything is baked fresh.</li>
-          <li>Pickup - Pickup your order from our store.</li>
-        </ol>
-      </section>
-      <section>
-        <div>
-          <h2>Catering Inquiry</h2>
-          <p>Have a special request or large order? We'd love to help.</p>
-        </div>
-        <div>
-          <div>Phone: (000) 000-0000</div>
-          <div>Address: 325 Broad Ave, Palisades Park, NJ 07650</div>
-          <div>Email: 000@gmail.com</div>
-          <div>Minimum Order: $50</div>
-          <div>Lead Time: 48 hours notice required.</div>
-        </div>
-      </section>
-      <section>
-        <h2>Frequently Asked Questions</h2>
-        <details>
-          <summary>How far in advance should I order?</summary>
-          <p>
-            We recommend at least 48-72 hours notice for standard catering
-            orders.
-          </p>
-        </details>
-        <details>
-          <summary>Can you accommodate allergies?</summary>
-          <p>Yes! Please notify us of any allergies when placing your order.</p>
-        </details>
-        <details>
-          <summary>How to contact us?</summary>
-          <p>
-            You can directly call now: (000) 000-0000. Or visit our store: 325
-            Broad Ave, Palisades Park, NJ 07650. We are excited to help you!
-          </p>
-        </details>
-      </section> */}
-    </main>
+          <h2>Frequently Asked Questions</h2>
+          <details>
+            <summary>How far in advance should I order?</summary>
+            <p>
+              We recommend at least 48-72 hours notice for standard catering
+              orders.
+            </p>
+          </details>
+          <details>
+            <summary>Can you accommodate allergies?</summary>
+            <p>Yes! Please notify us of any allergies when placing your order.</p>
+          </details>
+          <details>
+            <summary>How to contact us?</summary>
+            <p>
+              You can directly call now: (000) 000-0000. Or visit our store: 325
+              Broad Ave, Palisades Park, NJ 07650. We are excited to help you!
+            </p>
+          </details>
+        </section> */}
+      </main>
+    </div>
   );
 }
 
--- app/custom-cakes/page.tsx
+++ app/custom-cakes/page.tsx
@@ -4,108 +4,185 @@ type Props = {};
 
 function page({}: Props) {
   return (
-    <main>
-      <header>
-        <h1>Custom Cakes</h1>
-        <p>
-          Every celebration is unique and your cake should be too. From
-          birthdays to weddings, we create fully customized cakes designed to
-          match your vision, theme, and taste.
-        </p>
-      </header>
-      <section>
-        <h2>What You Can Customize</h2>
-        <section>
-          <h3>Cake Size</h3>
-          <ul>
-            <li>6", 8", 10" Round Cakes</li>
-            <li>Half & Full Sheet Cakes</li>
-            <li>Multi-Tier Wedding Cakes</li>
-          </ul>
-        </section>
-        <section>
-          <h3>Design</h3>
-          <ul>
-            <li>Minimal & Elegant</li>
-            <li>Floral & Romantic</li>
-            <li>Character & Themed Cakes</li>
-          </ul>
-        </section>
-        <section>
-          <h3>Flavors</h3>
-          <ul>
-            <li>Vanilla</li>
-            <li>Strawberry</li>
-            <li>Chocolate</li>
-            <li>Red Velvet</li>
-            <li>Lemon</li>
-            <li>Matcha</li>
-          </ul>
-        </section>
-        <section>
-          <h3>Fillings & Frosting</h3>
-          <ul>
-            <li>Fresh Cream</li>
-            <li>Buttercream</li>
-            <li>Cream Cheese Frosting</li>
-            <li>Fruit Fillings</li>
-            <li>Chocolate Ganache</li>
-          </ul>
-        </section>
-        <div>Flavor and design options may vary by season.</div>
-      </section>
-      <section>
-        <h2>Perfect For</h2>
-        <ul>
-          <li>Birthdays</li>
-          <li>Weddings & Engagements</li>
-          <li>Anniversaries</li>
-          <li>Baby Showers</li>
-          <li>Graduations</li>
-          <li>Corporate Events</li>
-        </ul>
-      </section>
-      <section>
-        <h2>How to Order</h2>
-        <ol>
-          <li>
-            Choose Your flavor - Think about the what kind of flavor cake you
-            want.
-          </li>
-          <li>
-            Deisgn Consultation - Contact us to confirm your design, flavor, and
-            pricing.
-          </li>
-          <li>Confirmation & Deposit - Orders are confirmed with a deposit.</li>
-          <li>Pickup - Your cake will be ready on your selected date.</li>
-        </ol>
-      </section>
-      <section>
-        <div>
-          <h2>Custom Cake Request</h2>
-          <p>We'd love to help.</p>
-        </div>
-        <div>
-          <div>Phone: (000) 000-0000</div>
-          <div>Address: 325 Broad Ave, Palisades Park, NJ 07650</div>
-          <div>Email: 000@gmail.com</div>
-        </div>
-      </section>
-      <section>
-        <h2>Frequently Asked Questions</h2>
-        <dialog>
-          <summary>How far in advance should I order?</summary>
+    <div className="wrapper">
+      <main className="flex flex-col gap-6 p-4">
+        <header>
+          <h1 className="text-[1.4rem] font-bold">Custom Cakes</h1>
           <p>
-            We recommend at least 48-72 hours notice for standard catering
-            orders.
+            Every celebration is unique and your cake should be too. From
+            birthdays to weddings, we create fully customized cakes designed to
+            match your vision, theme, and taste.
           </p>
-        </dialog>
-        <dialog>
-          <summary>Can you accommodate allergies?</summary>
-          <p>Yes! Please notify us of any allergies when placing your order.</p>
-        </dialog>
-      </section>
-    </main>
+        </header>
+
+        <section>
+          <h2 className="text-[1.1rem] italic">
+            Bring Any Design. Celebrate Any Moment
+          </h2>
+          <p>
+            From birthday celebrations to weddings and milestones, Palisades
+            Park Bakery brings your custom cake ideas to life with fresh
+            ingredients, any flavor you love, and fully personalized designs.
+          </p>
+        </section>
+
+        <section>
+          <h2 className="text-[1.1rem] italic">Ordering Guidelines</h2>
+          <div className="grid grid-cols-2 gap-4">
+            <div>
+              <div>Order at least 2 days in advance</div>
+              <p>
+                We handcraft each custom cake, so early notice helps us prepare
+                ingredients, design details, and specialized decorations.
+              </p>
+            </div>
+            <div>
+              <div>Place your order by phone or in-person</div>
+              <p>Just call or visit to speak with a staff member.</p>
+            </div>
+            <div>
+              <div>Provide basic information</div>
+              <ul className="list-disc list-inside pl-4">
+                <li>Cake size (up to full sheet or 10" round)</li>
+                <li>Flavor & filling</li>
+                <li>Any allergies</li>
+                <li>Date & time of pickup</li>
+                <li>Design ideas & occasion (optional)</li>
+              </ul>
+              <p></p>
+            </div>
+            <div>
+              <div>Pay in advance</div>
+              <p>
+                Once payment is confirmed, the order will be made ready for you.
+              </p>
+            </div>
+          </div>
+        </section>
+
+        <section>
+          <h2 className="text-[1.1rem] italic">Cake Options</h2>
+          <div className="flex flex-col gap-4">
+            <div>
+              <div>Sizes</div>
+              <div>
+                <p>Up to full sheet cakes</p>
+                <p>Up to 10" round cakes</p>
+                <p>Up to 2 tiers</p>
+              </div>
+            </div>
+            <div>
+              <div>Flavors</div>
+              <p>All flavors are available upon request.</p>
+            </div>
+            <div>
+              <div>Customization</div>
+              <p>Colors, decorations, and writing done to request</p>
+              <p>Accomodations available for basic dietary needs</p>
+            </div>
+          </div>
+        </section>
+
+        <section>
+          <h2 className="text-[1.1rem] italic">Place Your Group Order Today</h2>
+          <p>We'll help create the perfect cake for your celebration.</p>
+          <div>Call or Visit Us Now</div>
+        </section>
+
+        {/* <section>
+          <h2>What You Can Customize</h2>
+          <section>
+            <h3>Cake Size</h3>
+            <ul>
+              <li>6", 8", 10" Round Cakes</li>
+              <li>Half & Full Sheet Cakes</li>
+              <li>Multi-Tier Wedding Cakes</li>
+            </ul>
+          </section>
+          <section>
+            <h3>Design</h3>
+            <ul>
+              <li>Minimal & Elegant</li>
+              <li>Floral & Romantic</li>
+              <li>Character & Themed Cakes</li>
+            </ul>
+          </section>
+          <section>
+            <h3>Flavors</h3>
+            <ul>
+              <li>Vanilla</li>
+              <li>Strawberry</li>
+              <li>Chocolate</li>
+              <li>Red Velvet</li>
+              <li>Lemon</li>
+              <li>Matcha</li>
+            </ul>
+          </section>
+          <section>
+            <h3>Fillings & Frosting</h3>
+            <ul>
+              <li>Fresh Cream</li>
+              <li>Buttercream</li>
+              <li>Cream Cheese Frosting</li>
+              <li>Fruit Fillings</li>
+              <li>Chocolate Ganache</li>
+            </ul>
+          </section>
+          <div>Flavor and design options may vary by season.</div>
+        </section>
+        <section>
+          <h2>Perfect For</h2>
+          <ul>
+            <li>Birthdays</li>
+            <li>Weddings & Engagements</li>
+            <li>Anniversaries</li>
+            <li>Baby Showers</li>
+            <li>Graduations</li>
+            <li>Corporate Events</li>
+          </ul>
+        </section>
+        <section>
+          <h2>How to Order</h2>
+          <ol>
+            <li>
+              Choose Your flavor - Think about the what kind of flavor cake you
+              want.
+            </li>
+            <li>
+              Deisgn Consultation - Contact us to confirm your design, flavor, and
+              pricing.
+            </li>
+            <li>Confirmation & Deposit - Orders are confirmed with a deposit.</li>
+            <li>Pickup - Your cake will be ready on your selected date.</li>
+          </ol>
+        </section>
+        <section>
+          <div>
+            <h2>Custom Cake Request</h2>
+            <p>We'd love to help.</p>
+          </div>
+          <div>
+            <div>Phone: (000) 000-0000</div>
+            <div>Address: 325 Broad Ave, Palisades Park, NJ 07650</div>
+            <div>Email: 000@gmail.com</div>
+          </div>
+        </section>
+        <section>
+          <h2>Frequently Asked Questions</h2>
+          <dialog>
+            <summary>How far in advance should I order?</summary>
+            <p>
+              We recommend at least 48-72 hours notice for standard catering
+              orders.
+            </p>
+          </dialog>
+          <dialog>
+            <summary>Can you accommodate allergies?</summary>
+            <p>Yes! Please notify us of any allergies when placing your order.</p>
+          </dialog>
+        </section> */}
+      </main>
+    </div>
   );
 }
 
