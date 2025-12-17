8a874cd3c94b0dfb1f8385d0508317aed05ffda1
jessicason0 <jessicajson0@gmail.com>
2025-12-16 19:52:50 -0500

    edit: custom cake styling

--- app/custom-cakes/page.tsx
+++ app/custom-cakes/page.tsx
@@ -1,6 +1,24 @@
 import Image from "next/image";
 import React from "react";
+// import f11 from "../homepage/images/f11.png";
 import f9 from "../homepage/images/f9.jpg";
+import strawberry from "./images/strawberry.jpg";
+import chocolate from "./images/chocolate.jpg";
+import lemon from "./images/lemon.jpg";
+import vanilla from "./images/vanilla.jpg";
+// import carrot from "./images/carrot.jpg";
+import more from "./images/more.jpg";
+// import color from "./images/color.webp";
+import color2 from "./images/color2.jpg";
+import writing from "./images/writing.jpg";
+import decoration from "./images/decoration.avif";
+import prints from "./images/prints.webp";
+import rectangle from "./images/rectangle.jpg";
+import round from "./images/round.jpg";
+import round2 from "./images/round2.jpg";
+import tier from "./images/tier.jpg";
+import BulletPoint from "../_components/svg/BulletPoint";
+// import tier2 from "./images/tier2.webp";
 
 type Props = {};
 
@@ -34,161 +52,221 @@ function page({}: Props) {
       </header>
 
       <div className="wrapper p-4">
-        <section>
-          <h2 className="text-[1.1rem] italic">Ordering Guidelines</h2>
-          <div className="grid grid-cols-2 gap-4">
-            <div>
-              <div>Order at least 2 days in advance</div>
-              <p>
-                We handcraft each custom cake, so early notice helps us prepare
-                ingredients, design details, and specialized decorations.
-              </p>
-            </div>
-            <div>
-              <div>Place your order by phone or in-person</div>
-              <p>Just call or visit to speak with a staff member.</p>
-            </div>
-            <div>
-              <div>Provide basic information</div>
-              <ul className="list-disc list-inside pl-4">
-                <li>Cake size (up to full sheet or 10" round)</li>
-                <li>Flavor & filling</li>
-                <li>Any allergies</li>
-                <li>Date & time of pickup</li>
-                <li>Design ideas & occasion (optional)</li>
-              </ul>
-              <p></p>
-            </div>
-            <div>
-              <div>Pay in advance</div>
-              <p>
-                Once payment is confirmed, the order will be made ready for you.
-              </p>
-            </div>
-          </div>
-        </section>
-
-        <section>
-          <h2 className="text-[1.1rem] italic">Cake Options</h2>
-          <div className="flex flex-col gap-4">
-            <div>
-              <div>Sizes</div>
+        <div className="flex flex-col gap-8">
+          <section className="flex flex-col gap-4">
+            <h2 className="text-[1.1rem] italic">Ordering Guidelines</h2>
+            <div className="grid grid-cols-2 gap-6 max-md:grid-cols-1 max-md:gap-4">
               <div>
-                <p>Up to full sheet cakes</p>
-                <p>Up to 10" round cakes</p>
-                <p>Up to 2 tiers</p>
+                <div className="flex items-center gap-1">
+                  <BulletPoint />
+                  <div className="underline">
+                    Order at least 2 days in advance
+                  </div>
+                </div>
+                <ul className="list-disc list-outside pl-10">
+                  <li>
+                    We handcraft each custom cake, so early notice helps us
+                    prepare ingredients, design details, and specialized
+                    decorations.
+                  </li>
+                </ul>
+              </div>
+              <div>
+                <div className="flex items-center gap-1">
+                  <BulletPoint />
+                  <div className="underline">
+                    Place your order by phone or in-person
+                  </div>
+                </div>
+                <ul className="list-disc list-outside pl-10">
+                  <li>Just call or visit to speak with a staff member.</li>
+                </ul>
+              </div>
+              <div>
+                <div className="flex items-center gap-1">
+                  <BulletPoint />
+                  <div className="underline">Provide basic information</div>
+                </div>
+                <ul className="list-disc list-outside pl-10">
+                  <li>Cake size (up to full sheet or 10" round)</li>
+                  <li>Flavor & filling</li>
+                  <li>Any allergies</li>
+                  <li>Date & time of pickup</li>
+                  <li>Design ideas & occasion (optional)</li>
+                </ul>
+                <p></p>
+              </div>
+              <div>
+                <div className="flex items-center gap-1">
+                  <BulletPoint />
+                  <div className="underline">Pay in advance</div>
+                </div>
+                <ul className="list-disc list-outside pl-10">
+                  <li>
+                    Once payment is confirmed, the order will be made ready for
+                    you.
+                  </li>
+                </ul>
               </div>
             </div>
-            <div>
-              <div>Flavors</div>
-              <p>All flavors are available upon request.</p>
-            </div>
-            <div>
-              <div>Customization</div>
-              <p>Colors, decorations, and writing done to request</p>
-              <p>Accomodations available for basic dietary needs</p>
-            </div>
-          </div>
-        </section>
+          </section>
 
-        <section>
-          <h2 className="text-[1.1rem] italic">Place Your Group Order Today</h2>
-          <p>We'll help create the perfect cake for your celebration.</p>
-          <div>Call or Visit Us Now</div>
-        </section>
+          <section className="flex flex-col gap-4 p-6 bg-(--clr-background4)">
+            <div>
+              <h2 className="text-[1.1rem] italic font-semibold">
+                Cake Options
+              </h2>
+              <p>Choose your desired custom cake</p>
+            </div>
+            <div className="flex flex-col gap-8">
+              <div className="flex flex-col gap-4 items-center">
+                <div className="font-semibold text-lg">1. Pick your size</div>
+                <div className="grid grid-cols-3 gap-6">
+                  <div className="flex flex-col gap-2">
+                    <div className="relative">
+                      <Image
+                        src={rectangle}
+                        alt="Strawberry Flavor Cake"
+                        className="object-cover rounded-full w-32 aspect-square"
+                      />
+                    </div>
+                    <p className="text-center text-sm text-(--clr-logo)">
+                      up to full sheet
+                    </p>
+                  </div>
+                  <div className="flex flex-col gap-2">
+                    <div className="relative">
+                      <Image
+                        src={round2}
+                        alt="Chocolate Flavor Cake"
+                        className="object-cover rounded-full w-32 aspect-square"
+                      />
+                    </div>
+                    <p className="text-center text-sm text-(--clr-logo)">
+                      up to 10" round
+                    </p>
+                  </div>
+                  <div className="flex flex-col gap-2">
+                    <div className="relative">
+                      <Image
+                        src={tier}
+                        alt="More Flavor Cakes"
+                        className="object-cover rounded-full w-32 aspect-square"
+                      />
+                    </div>
+                    <p className="text-center text-sm text-(--clr-logo)">
+                      up to 2 tiers
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="flex flex-col gap-4 items-center">
+                <p className="font-semibold text-lg">2. Pick your flavor</p>
+                <div className="grid grid-cols-5 gap-6 max-md:grid-cols-3">
+                  <div className="relative">
+                    <Image
+                      src={strawberry}
+                      alt="Strawberry Flavor Cake"
+                      className="object-cover rounded-full w-32 aspect-square"
+                    />
+                  </div>
+                  <div className="relative">
+                    <Image
+                      src={chocolate}
+                      alt="Chocolate Flavor Cake"
+                      className="object-cover rounded-full w-32 aspect-square"
+                    />
+                  </div>
+                  <div className="relative">
+                    <Image
+                      src={lemon}
+                      alt="More Flavor Cakes"
+                      className="object-cover rounded-full w-32 aspect-square"
+                    />
+                  </div>
+                  <div className="relative">
+                    <Image
+                      src={vanilla}
+                      alt="More Flavor Cakes"
+                      className="object-cover rounded-full w-32 aspect-square"
+                    />
+                  </div>
+                  <div className="relative">
+                    <Image
+                      src={more}
+                      alt="More Flavor Cakes"
+                      className="object-cover rounded-full w-32 aspect-square"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="flex flex-col gap-4 items-center">
+                <div className="font-semibold text-lg">
+                  3. Pick your customization
+                </div>
 
-        {/* <section>
-          <h2>What You Can Customize</h2>
-          <section>
-            <h3>Cake Size</h3>
-            <ul>
-              <li>6", 8", 10" Round Cakes</li>
-              <li>Half & Full Sheet Cakes</li>
-              <li>Multi-Tier Wedding Cakes</li>
-            </ul>
+                <div className="grid grid-cols-4 gap-6 flex-2 max-md:grid-cols-3">
+                  <div className="flex flex-col gap-2">
+                    <div className="relative">
+                      <Image
+                        src={color2}
+                        alt="Strawberry Flavor Cake"
+                        className="object-cover rounded-full w-32 aspect-square"
+                      />
+                    </div>
+                    <p className="text-center text-sm text-(--clr-logo)">
+                      color
+                    </p>
+                  </div>
+                  <div className="flex flex-col gap-2">
+                    <div className="relative">
+                      <Image
+                        src={writing}
+                        alt="Chocolate Flavor Cake"
+                        className="object-cover rounded-full w-32 aspect-square"
+                      />
+                    </div>
+                    <p className="text-center text-sm text-(--clr-logo)">
+                      writing
+                    </p>
+                  </div>
+                  <div className="flex flex-col gap-2">
+                    <div className="relative">
+                      <Image
+                        src={decoration}
+                        alt="More Flavor Cakes"
+                        className="object-cover rounded-full w-32 aspect-square"
+                      />
+                    </div>
+                    <p className="text-center text-sm text-(--clr-logo)">
+                      decoration
+                    </p>
+                  </div>
+                  <div className="flex flex-col gap-2">
+                    <div className="relative">
+                      <Image
+                        src={prints}
+                        alt="More Flavor Cakes"
+                        className="object-cover rounded-full w-32 aspect-square"
+                      />
+                    </div>
+                    <p className="text-center text-sm text-(--clr-logo)">
+                      printed photos
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
           </section>
+
           <section>
-            <h3>Design</h3>
-            <ul>
-              <li>Minimal & Elegant</li>
-              <li>Floral & Romantic</li>
-              <li>Character & Themed Cakes</li>
-            </ul>
+            <h2 className="text-[1.1rem] italic">
+              Place Your Group Order Today
+            </h2>
+            <p>We'll help create the perfect cake for your celebration.</p>
+            <div>Call or Visit Us Now</div>
           </section>
-          <section>
-            <h3>Flavors</h3>
-            <ul>
-              <li>Vanilla</li>
-              <li>Strawberry</li>
-              <li>Chocolate</li>
-              <li>Red Velvet</li>
-              <li>Lemon</li>
-              <li>Matcha</li>
-            </ul>
-          </section>
-          <section>
-            <h3>Fillings & Frosting</h3>
-            <ul>
-              <li>Fresh Cream</li>
-              <li>Buttercream</li>
-              <li>Cream Cheese Frosting</li>
-              <li>Fruit Fillings</li>
-              <li>Chocolate Ganache</li>
-            </ul>
-          </section>
-          <div>Flavor and design options may vary by season.</div>
-        </section>
-        <section>
-          <h2>Perfect For</h2>
-          <ul>
-            <li>Birthdays</li>
-            <li>Weddings & Engagements</li>
-            <li>Anniversaries</li>
-            <li>Baby Showers</li>
-            <li>Graduations</li>
-            <li>Corporate Events</li>
-          </ul>
-        </section>
-        <section>
-          <h2>How to Order</h2>
-          <ol>
-            <li>
-              Choose Your flavor - Think about the what kind of flavor cake you
-              want.
-            </li>
-            <li>
-              Deisgn Consultation - Contact us to confirm your design, flavor, and
-              pricing.
-            </li>
-            <li>Confirmation & Deposit - Orders are confirmed with a deposit.</li>
-            <li>Pickup - Your cake will be ready on your selected date.</li>
-          </ol>
-        </section>
-        <section>
-          <div>
-            <h2>Custom Cake Request</h2>
-            <p>We'd love to help.</p>
-          </div>
-          <div>
-            <div>Phone: (000) 000-0000</div>
-            <div>Address: 325 Broad Ave, Palisades Park, NJ 07650</div>
-            <div>Email: 000@gmail.com</div>
-          </div>
-        </section>
-        <section>
-          <h2>Frequently Asked Questions</h2>
-          <dialog>
-            <summary>How far in advance should I order?</summary>
-            <p>
-              We recommend at least 48-72 hours notice for standard catering
-              orders.
-            </p>
-          </dialog>
-          <dialog>
-            <summary>Can you accommodate allergies?</summary>
-            <p>Yes! Please notify us of any allergies when placing your order.</p>
-          </dialog>
-        </section> */}
+        </div>
       </div>
     </main>
   );
