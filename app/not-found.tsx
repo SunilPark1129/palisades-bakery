0f0cf37657808f2f95b0c53395082e6845883083
Sunil Park <sunilpark1129@gmail.com>
2025-12-22 18:53:47 -0500

    edit: style

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 function NotFound() {
   return (
-    <div className="flex flex-col items-center gap-8 p-4 mb-16">
+    <div className="flex flex-col items-center gap-8 p-4 my-16">
       <Image
         src={
           "https://plus.unsplash.com/premium_photo-1732333561323-6c9dbe677219?q=80&w=747&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
@@ -16,7 +17,11 @@ function NotFound() {
       ></Image>
       <div className="flex flex-col gap-2 text-center">
         <h2 className="text-2xl">Uh Oh...Page Not Found</h2>
-        <p>Click on Logo to go to Home Page</p>
+        <span>
+          <Link href={"/"} className="text-[#0825e4]">
+            Click here to go to Home Page
+          </Link>
+        </span>
       </div>
     </div>
   );
