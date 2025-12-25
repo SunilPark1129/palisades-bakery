cf6bb38cf92fa6f7be09bf6ac0c08d9c494e27f2
jessicason0 <jessicajson0@gmail.com>
2025-12-25 16:46:07 -0500

    edit: admin modal + styling

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -3,6 +3,9 @@ import { useRouter } from "next/navigation";
 import React, { useEffect, useState } from "react";
 import AddForm from "./AddForm";
 import { IProduct } from "@/models/Product";
+import Trash from "../_components/svg/Trash";
+import Edit from "../_components/svg/Edit";
+import Alert from "../_components/svg/Alert";
 
 type Props = {};
 type ProductType = "cake" | "bread" | "cookie" | "pie";
@@ -13,6 +16,9 @@ function page({}: Props) {
   const [products, setProducts] = useState<IProduct[]>([]);
   const [isModalOn, setIsModalOn] = useState<boolean>(false);
   const [selectedProduct, setSelectedProduct] = useState<ProductType>("cake");
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState<boolean>(false);
+
+  const [itemId, setItemId] = useState<string>("");
 
   async function handleLogout() {
     await fetch("/api/auth/logout", { method: "POST" });
@@ -23,7 +29,12 @@ function page({}: Props) {
     setIsModalOn(true);
   }
 
-  async function handleDeleteItem(id: string) {
+  function handleDeleteItem(id: string) {
+    setIsDeleteModalOpen(true);
+    setItemId(id);
+  }
+
+  async function handleConfirmDelete(id: string) {
     await fetch("/api/categories", {
       method: "DELETE",
       headers: {
@@ -32,6 +43,7 @@ function page({}: Props) {
       body: JSON.stringify({ id }),
       credentials: "include",
     });
+    setIsDeleteModalOpen(false);
   }
 
   useEffect(() => {
@@ -51,14 +63,17 @@ function page({}: Props) {
 
   return (
     <div className="wrapper">
-      <div>
-        <button onClick={handleAddNew}>Add New</button>
-        <button onClick={handleLogout}>Logout</button>
+      <div className="flex flex-col gap-4 p-4">
         <div>
-          {isModalOn && (
-            <AddForm setProducts={setProducts} setIsModalOn={setIsModalOn} />
-          )}
+          <button onClick={handleAddNew}>Add New</button>
+          <button onClick={handleLogout}>Logout</button>
+          <div>
+            {isModalOn && (
+              <AddForm setProducts={setProducts} setIsModalOn={setIsModalOn} />
+            )}
+          </div>
         </div>
+
         <div className="flex gap-4">
           {productTitles.map((item) => (
             <button
@@ -70,20 +85,50 @@ function page({}: Props) {
             </button>
           ))}
         </div>
+
         <div className="grid gap-4 sm:grid-cols-2">
-          {products.map((item, idx) => (
-            <div key={item._id}>
+          {products.map((item) => (
+            <div key={item._id} className="flex flex-col">
               <div>{item.title}</div>
-              <button className="cursor-pointer">edit</button>
-              <button
-                onClick={() => handleDeleteItem(item._id!)}
-                className="cursor-pointer"
-              >
-                delete
-              </button>
+              <div>
+                <button className="cursor-pointer p-1 rounded-full hover:bg-(--clr-accent)">
+                  <Edit />
+                </button>
+                <button
+                  onClick={() => handleDeleteItem(item._id!)}
+                  className="cursor-pointer p-1 rounded-full hover:bg-(--clr-accent)"
+                >
+                  <Trash />
+                </button>
+              </div>
             </div>
           ))}
         </div>
+
+        {isDeleteModalOpen && (
+          <div className="fixed left-1/2 transform -translate-x-1/2 flex flex-col items-center gap-4 bg-gray-100 p-10 rounded-xl">
+            <Alert />
+            <div className="font-semibold">DELETE [삭제]</div>
+            <div className="flex flex-col gap-1 text-center">
+              <div>Are you sure you want to delete this item?</div>
+              <div>이 항목을 삭제하시겠습니까?</div>
+            </div>
+            <div className="flex gap-8 mt-2">
+              <button
+                onClick={() => setIsDeleteModalOpen(false)}
+                className="w-20 bg-gray-300 p-1 rounded cursor-pointer"
+              >
+                Cancel
+              </button>
+              <button
+                onClick={() => handleConfirmDelete(itemId)}
+                className="w-20 bg-red-500 text-(--clr-background) p-1 rounded cursor-pointer"
+              >
+                Delete
+              </button>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
