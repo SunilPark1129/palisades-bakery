7127eea97e3c9276b9bbb160ad6cea3f501e24aa
Sunil Park <sunilpark1129@gmail.com>
2026-01-06 05:16:35 -0500

    add: middleware range

--- proxy.ts
+++ proxy.ts
@@ -3,20 +3,38 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function proxy(request: NextRequest) {
   const token = request.cookies.get("token")?.value;
+  const { pathname } = request.nextUrl;
+  console.log("path:", pathname);
 
-  if (!token) {
-    return NextResponse.redirect(new URL("/login", request.url));
+  if (pathname === "/login") {
+    if (!token) {
+      return NextResponse.next();
+    }
+
+    const user = await verifyToken(token);
+
+    if (user) {
+      return NextResponse.redirect(new URL("/admin", request.url));
+    }
+
+    return NextResponse.next();
   }
 
-  const user = await verifyToken(token);
+  if (pathname.startsWith("/admin")) {
+    if (!token) {
+      return NextResponse.redirect(new URL("/login", request.url));
+    }
 
-  if (!user) {
-    return NextResponse.redirect(new URL("/login", request.url));
+    const user = await verifyToken(token);
+
+    if (!user) {
+      return NextResponse.redirect(new URL("/login", request.url));
+    }
   }
 
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/admin/:path*"],
+  matcher: ["/admin/:path*", "/login"],
 };
