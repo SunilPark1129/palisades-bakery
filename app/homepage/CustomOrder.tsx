a10725bdb727c71c266398397dd020041b89fe77
Sunil Park <sunilpark1129@gmail.com>
2026-01-14 13:40:11 -0500

    add: image

--- app/homepage/CustomOrder.tsx
+++ app/homepage/CustomOrder.tsx
@@ -9,7 +9,9 @@ function CustomOrder() {
     <section className="flex w-full max-[720px]:flex-col gap-4">
       <div className="relative w-full max-w-xl min-h-64 max-[720px]:max-w-full aspect-video rounded overflow-hidden">
         <Image
-          src={customCakeImg}
+          src={
+            "https://images.unsplash.com/photo-1586985289688-ca3cf47d3e6e?q=80&w=774&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          }
           alt="Custom Cake Ordering"
           fill
           loading="lazy"
--- app/homepage/Hero.tsx
+++ app/homepage/Hero.tsx
@@ -1,13 +1,14 @@
 import Image from "next/image";
-import heroImg from "./images/i8.jpg";
+// import heroImg from "./images/i8.jpg";
+import heroImg from "./images/k12.jpg";
 
 function Hero() {
   return (
     <section className="flex flex-col">
       <div className="w-full aspect-video max-h-150 min-h-85 relative">
         <Image
-          // className="object-cover saturate-50 contrast-35"
-          className="object-cover"
+          className="object-cover contrast-75"
+          // className="object-cover"
           src={heroImg}
           fill
           alt="Palisades Park Bakery Banner"
