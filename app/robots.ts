cbf9ac10e938f87ddf2b82e5787fea8541ef2cb6
Sunil Park <sunilpark1129@gmail.com>
2026-01-23 06:23:38 -0500

    sitemap domain update

--- app/robots.ts
+++ app/robots.ts
@@ -7,17 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: "/",
       disallow: ["/api/", "/_next/", "/admin/", "/drafts/"],
     },
-    sitemap: "http://localhost:3000/sitemap.xml",
+    sitemap: "www.palisadesparkbakery.com.xml",
   };
 }
-// palisadesparkbakery
-
-// # Allow all search engines to crawl public pages
-// User-agent: *
-// Disallow: /api/
-// Disallow: /_next/
-// Disallow: /admin/
-// Disallow: /drafts/
-
-// # Sitemap
-// Sitemap: https://www.palisadesparkbakery.com/sitemap.xml
