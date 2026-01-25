6c0c59c41ff1544fb537f3960e169a22f936ec2b
Sunil Park <sunilpark1129@gmail.com>
2026-01-24 21:57:41 -0500

    fix: sitemap address

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: "/",
       disallow: ["/api/", "/_next/", "/admin/", "/drafts/"],
     },
-    sitemap: "www.palisadesparkbakerynj.com.xml",
+    sitemap: "www.palisadesparkbakerynj.com/sitemap.xml",
   };
 }
